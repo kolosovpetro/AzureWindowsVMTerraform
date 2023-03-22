cd9680db90e08cb4713a0f8f1442a515cc32cd42
kolosovpetro <kolosovp94@gmail.com>
2023-03-22 20:08:53 +0100

    software installed

--- createAzureVM.ps1
+++ createAzureVM.ps1
@@ -1,8 +1,8 @@
-$prefix = "04";   `
-  $rgName = "rg-test-$prefix";   `
-  $vmName = "vm-test$prefix";   `
-  $storageAccountName = "stortestvm$prefix";   `
-  $containerName = "container-test-vm$prefix";
+$prefix = "04";    `
+   $rgName = "rg-test-$prefix";    `
+   $vmName = "vm-test$prefix";    `
+   $storageAccountName = "stortestvm$prefix";    `
+   $containerName = "container-test-vm$prefix";
 
 az group create -n $rgName -l "northeurope";
 
@@ -27,12 +27,7 @@ az vm create `
     --public-ip-sku "Standard" `
     --size "Standard_B4ms"
 
-Set-AzVMCustomScriptExtension -ResourceGroupName <resourceGroupName> `
-    -VMName $vmName `
-    -Location "northeurope" `
-    -FileUri <fileUrl> `
-    -Run 'myScript.ps1' `
-    -Name DemoScriptExtension
+az vm open-port -g $rgName -n $vmName --port 80 --priority 1010
 
 az storage account create `
     --name $storageAccountName `
@@ -45,5 +40,14 @@ az storage container create `
     --name $containerName `
     --account-name $storageAccountName `
     --public-access "off"
+
+$fileUrl = "https://stortestvm04.blob.core.windows.net/container-test-vm04/installSoftware.ps1?sp=r&st=2023-03-22T18:48:20Z&se=2023-03-23T02:48:20Z&spr=https&sv=2021-12-02&sr=b&sig=z5kmJy5bwHDSmhqv2NUC455thItzSmx9ZGOicaTx%2FGw%3D"
+
+Set-AzVMCustomScriptExtension -ResourceGroupName $rgName `
+    -VMName $vmName `
+    -Location "northeurope" `
+    -FileUri $fileUrl `
+    -Run 'installSoftware.ps1' `
+    -Name "DemoScriptExtension"
 
 az group delete -n $rgName -y
